b86378cafb6c7963b88838eae18c41e93c5019f0
Shiva Gupta <48547715+TeDDyDev@users.noreply.github.com>
2021-09-27 23:29:48 +0530

    Update Day 8 Dictionaries and Maps.cpp

--- Day 8/Day 8 Dictionaries and Maps.cpp
+++ Day 8/Day 8 Dictionaries and Maps.cpp
@@ -12,4 +12,20 @@ int main() {
     string name,num,query[100];
     map<string,string> phone;
     cin>>n;
-   
+    for(int i=0;i<n;i++)  
+    {
+    cin>>name;
+    cin>>num;
+    phone.insert(pair<string,string>(name,num));
+    }
+  
+    map<string,string>::iterator it;
+  while(cin>>name) 
+    {
+        if (phone.find(name)!=phone.end())
+            cout<<name<<"="<<phone.find(name)->second<<endl;
+        else
+            cout<<"Not found"<<endl;
+    }
+    return 0;
+}
